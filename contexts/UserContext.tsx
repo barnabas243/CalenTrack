a2ca7ca342e441e87d59897ea58e2b15c3416d65
Nicholas-Chow-hon-kit <nicholaschowhonkit@gmail.com>
2024-08-15 19:38:26 +0800

    Profile Setup mandatory update

--- contexts/UserContext.tsx
+++ contexts/UserContext.tsx
@@ -13,6 +13,8 @@ export const UserContext = createContext<UserContextType>({
   isLoading: true,
 });
 
+export const useAuth = () => React.useContext(UserContext);
+
 // Context provider component
 export const AuthProvider = ({children}: UserProviderProps) => {
   const [session, setSession] = useState<Session | null>(null);
